a28f0534493b1e346da3833e6cb14bfc6d0cf8ac
danielmachado <danielmachadopintos@gmail.com>
2025-08-05 11:28:07 -0300

    feat: aprimora endpoints da API para gerenciamento de itens, adiciona validações e ajusta resposta JSON

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require 'pry'
 MONGO_URL = ENV.fetch('MONGO_URL', 'mongodb://localhost:27017/mydb')
 DB = Mongo::Client.new(MONGO_URL)
 
+# Define the Sinatra application
 class App < Sinatra::Base
   get '/' do
     'Sinatra MongoDB API'
@@ -14,16 +15,26 @@ class App < Sinatra::Base
 
   get '/items' do
     content_type :json
-    DB[:items].find.to_a.to_json
+    docs = DB[:items].find.map { |d| d.merge('_id' => d['_id'].to_s) }
+    docs.to_json
   end
 
   post '/items' do
     content_type :json
-    data = JSON.parse(request.body.read)
-    binding.pry
-    result = DB[:items].insert_one(data)
+    body = request.body.read
+    halt 400, { error: 'Empty body' }.to_json if body.strip.empty?
+
+    begin
+      data = JSON.parse(body)
+    rescue JSON::ParserError
+      halt 400, { error: 'Invalid JSON' }.to_json
+    end
+
+    halt 400, { error: 'Expected an array of objects' }.to_json unless data.is_a?(Array)
+
+    result = DB[:items].insert_many(data)
     status 201
-    { id: result.inserted_id.to_s }.to_json
+    { ids: result.inserted_ids.map(&:to_s) }.to_json
   end
 end
 
